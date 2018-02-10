6cf788da432f769bb6c16534e86042c1b5db3283
EduardoSantosGit <eduardosantos058@gmail.com>
2018-02-10 20:11:53 -0200

    add example call netstat async

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import netstat from './netstat'
+/*
+let r = new netstat().executeCommand()
 
+r.then((result) => {
+    console.log('complete ', result)
+}).catch((error) => {
+    console.log(error)
+});
+*/
+
+let r = new netstat().teste()
+console.log(r)
 export {
-
+    r
 }
--- src/netstat.js
+++ src/netstat.js
@@ -1,11 +1,12 @@
-import { exec } from 'child_process'
+import { exec, spawnSync } from 'child_process'
 import co from 'co'
 
 export default class NetStat {
 
     async executeCommand(){        
         return new Promise((resolve, reject) => {
-            exec('netstat', (error, stdout, stderr) => {
+            exec('netstat -a', (error, stdout, stderr) => {
+                console.log(stdout)
                 if(error)
                     reject(err)
                 
@@ -13,4 +14,12 @@ export default class NetStat {
             })
         })
     }
+
+    netstat(){
+        let ls = spawnSync('netstat', ['-a'], { encoding : 'utf8', timeout: 1000 });       
+        //console.log('stdout here: \n' + ls.stdout);
+        return ls.stdout
+    }
+
+
 }
