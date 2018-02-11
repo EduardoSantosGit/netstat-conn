ea33278395c4f84821058641530b3bee3ba6a9f4
EduardoSantosGit <eduardosantos058@gmail.com>
2018-02-11 20:05:27 -0200

    add test command netstat

--- src/command.js
+++ src/command.js
@@ -3,7 +3,7 @@ import { exec, spawnSync } from 'child_process'
 export default class Command {
 
     commandNetstat(args, options){
-        let ls = spawnSync('netstat', args, { encoding : 'utf8', timeout: 1000 });      
+        let ls = spawnSync('netstat', [args], { encoding : 'utf8', timeout: 1000 });      
         return ls.stdout
     }
 }
--- test/command.spec.js
+++ test/command.spec.js
@@ -1,7 +1,11 @@
 import Command from '../src/command'
 import { expect } from 'chai'
 
-describe('netstat tests', () => {
+describe('Command tests', () => {
 
+    it('test method commandNetstat param -a return item includes address', () => { 
+        let ret = new Command().commandNetstat('-a')
+        expect(ret).to.includes('Local Address')
+    })
     
 })    
