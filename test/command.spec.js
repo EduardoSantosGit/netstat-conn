7f8060975c217a14de5d4db16760f6d142f6a9d0
EduardoSantosGit <eduardosantos058@gmail.com>
2018-02-11 20:12:10 -0200

    add method test netstat -n

--- test/command.spec.js
+++ test/command.spec.js
@@ -6,6 +6,8 @@ describe('Command tests', () => {
     it('test method commandNetstat param -a return item includes address', () => { 
         let ret = new Command().commandNetstat('-a')
         expect(ret).to.includes('Local Address')
+        expect(ret).to.includes('Foreign')
+        expect(ret).to.includes('State')
     })
     
     it('test method commandNetstat param -e return item includes texts bytes', () => { 
@@ -13,5 +15,13 @@ describe('Command tests', () => {
         expect(ret).to.includes('Bytes')
         expect(ret).to.includes('Received')
         expect(ret).to.includes('Sent')
+    })
+
+    it('test method commandNetstat param -n return item includes texts proto', () => { 
+        let ret = new Command().commandNetstat('-n')
+        expect(ret).to.includes('Proto')
+        expect(ret).to.includes('Local Address')
+        expect(ret).to.includes('Foreign')
+        expect(ret).to.includes('State')
     })
 })    
