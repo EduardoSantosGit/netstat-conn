02d8bdf1782926f78b0e9f7a350322ca56e326da
EduardoSantosGit <eduardosantos058@gmail.com>
2018-02-13 14:57:56 -0200

    alter logic init and remove parser

--- src/parser.js
+++ src/parser.js
@@ -13,35 +13,14 @@ export default class Parser {
         var rl = readline.createInterface({
             input: bufferStream,
         });
-        
+
         let linearr = []
         rl.on('line', (line) => {
-            linearr = line.split(/\s+/)
-            linearr.forEach(x => {
-                if(Number.isInteger(Number(x))){
-                     
-                }
-            });
+            linearr = line.split(/\s+/)      
+           
         });
-        
-        return 
-    }
 
-    parser(text, indexOn, indexLast) {
-
-        let textString = JSON.stringify(text);
-
-        let firstIndex = textString.lastIndexOf(indexOn);
-
-        firstIndex = firstIndex + indexOn.length;
-
-        let lastIndex = textString.indexOf(indexLast);
-
-        let exit = lastIndex - firstIndex;
-
-        let block = textString.substring(firstIndex, exit);
-
-        return block;
+        return
     }
 
 }
