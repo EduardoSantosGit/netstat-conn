99dee33641c3564f01992d06a2255918630ad995
EduardoSantosGit <eduardosantos058@gmail.com>
2018-02-17 20:52:05 -0200

    add method test json not null parsere

--- test/parser.spec.js
+++ test/parser.spec.js
@@ -19,5 +19,18 @@ describe('parser tests', () => {
         expect(jsonString).to.include("}");
     })
 
+    it('test method parserArgE params -e return json not null length valid', async () => {
+
+        let body = new Command().commandNetstat('-e')
+        let parser = new Parser();
+        let ret = await parser.parserArgE(body)
+        
+        expect(ret).to.not.null
+        expect(ret.length).to.equal(6)
+        ret.map(x => {
+            expect(x).to.not.null
+        })       
+    })
+
     
 })
