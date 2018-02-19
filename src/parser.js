ff4004a637ead07ec62e59704caa0b63050557fd
EduardoSantosGit <eduardosantos058@gmail.com>
2018-02-19 13:13:48 -0300

    add method parser netstat -s init logic

--- src/parser.js
+++ src/parser.js
@@ -179,6 +179,28 @@ export default class Parser {
 
     }
 
+    async parserArgS(body) {
+        var buf = new Buffer(body);
+        var bufferStream = new stream.PassThrough();
+        bufferStream.end(buf);
+
+        var rl = readline.createInterface({
+            input: bufferStream,
+        });
+
+        let linearr = []
+        await rl.on('line', (line) => {
+            linearr.push(line)
+        });
+
+        let data = []
+        linearr.forEach(x => {
+            data.push(x.split(/\s+/))
+        })
+    }    
+
+
+
 }
 
 Array.prototype.equals = function (array) {
