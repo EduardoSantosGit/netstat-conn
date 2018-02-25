05e525054563935a00432150b22463ea5b5d9a08
EduardoSantosGit <eduardosantos058@gmail.com>
2018-02-24 21:04:59 -0300

    add test method validate tcp ipv6 netstat -v json

--- test/parser.spec.js
+++ test/parser.spec.js
@@ -220,6 +220,20 @@ describe('parser tests', () => {
         expect(json).to.include("value")
     });
 
+    it('test method parserArgS return TcpIPv6 keys json valid', async () => {
+
+        let body = new Command().commandNetstat('-s')
+        let parser = new Parser();
+        let ret = await parser.parserArgS(body)
+        
+        let json = JSON.stringify(ret.TcpIPv6[0])
+
+        expect(json).to.include("{")
+        expect(json).to.include("}")
+        expect(json).to.include("name")
+        expect(json).to.include("value")
+    });
+
     async function auxFormatCmd(body){
         var buf = new Buffer(body);
         var bufferStream = new stream.PassThrough();
